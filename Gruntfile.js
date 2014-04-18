1327cba4a347ebd6211e51215142312e5f8c949c
Mike <mike@xpressivecode.com>
2014-04-18 10:48:57 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,166 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // show elapsed time
+    require('time-grunt')(grunt);
+
+    // load all grunt tasks automatically instead of manually including
+    require('load-grunt-tasks')(grunt);
+
+
+    grunt.initConfig({
+        templates: {
+            main: {
+                options: {
+                    src: 'dev',
+                    templateDir: 'dev/templates',
+                    dest: 'release'
+                }
+            }
+        },
+
+        connect: {
+            options: {
+                port: 3000,
+                base: 'release',
+                hostname: '*',
+                livereload: 9000
+            },
+            server: {}            
+        },
+
+        watch: {
+           livereload: {
+                options: {
+                    livereload: { port: 9000 }
+                },
+                files: ['dev/**/*.html', 'dev/**/*.scss', 'bower_components/bootstrap-sass-official/vendor/assets/stylesheets/bootstrap/bootstrap.scss'],
+                tasks: ['base']
+           }
+        },
+
+        open: {
+            server: {
+                path: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+
+        clean: {
+            all: ['tmp', 'release'],
+            temp: ['tmp']
+        },
+
+        copy: {
+            bootstrap: {
+                files: [
+                    { expand: true, src: ['bower_components/bootstrap-sass-official/vendor/assets/stylesheets/**', '!bower_components/bootstrap-sass-official/vendor/assets/stylesheets/**/bootstrap.scss'], dest: 'tmp/scss/', flatten: true, filter: 'isFile' }
+                ]
+            },
+
+            jquery: {
+                files: [
+                    { src: 'bower_components/jquery/dist/jquery.min.js', dest: 'tmp/js/min/jquery.min.js' }
+                ]
+            },
+
+            css: {
+                files: [
+                    { expand: true, src: ['dev/assets/scss/**'], dest: 'tmp/scss/', flatten: true, filter: 'isFile' }
+                ]
+            },
+
+            js: {
+                files: [
+                    { expand: true, src: ['dev/assets/js/*.js'], dest: 'tmp/js/', flatten: true, filter: 'isFile' }
+                ]
+            }
+        },
+
+        sass: {
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'tmp/scss',
+                    src: ['bootstrap.scss', 'main.scss'],
+                    dest: 'tmp/css',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        concat: {
+            bootstrap: {
+                options: {
+                    path: 'bower_components/bootstrap-sass-official/vendor/assets/javascripts/bootstrap/'
+                },
+                src: [
+                    '<%= concat.bootstrap.options.path %>affix.js',
+                    '<%= concat.bootstrap.options.path %>alert.js',
+                    '<%= concat.bootstrap.options.path %>button.js',
+                    '<%= concat.bootstrap.options.path %>carousel.js',
+                    '<%= concat.bootstrap.options.path %>collapse.js',
+                    '<%= concat.bootstrap.options.path %>dropdown.js',
+                    '<%= concat.bootstrap.options.path %>tab.js',
+                    '<%= concat.bootstrap.options.path %>transition.js',
+                    '<%= concat.bootstrap.options.path %>scrollspy.js',
+                    '<%= concat.bootstrap.options.path %>modal.js',
+                    '<%= concat.bootstrap.options.path %>tooltip.js',
+                    '<%= concat.bootstrap.options.path %>popover.js'
+                ],
+                dest: 'tmp/js/bootstrap.js'
+            },
+
+            css: {
+                src: ['tmp/css/bootstrap.css', 'tmp/css/*.css'],
+                dest: 'tmp/css/main.css'
+            },
+
+            js:  {
+                src: ['tmp/js/min/jquery.min.js', 'tmp/js/min/*.js'],
+                dest: 'release/js/main.min.js'
+            }
+        },
+
+        cssmin: {
+            options: {
+                keepBreaks: true
+            },
+            minify: {
+                expand: true,
+                cwd: 'tmp/css',
+                src: ['main.css'],
+                dest: 'release/css',
+                ext: '.min.css'
+            }
+        },
+
+        uglify: {
+            js: {
+                expand: true,
+                cwd: 'tmp/js',
+                src: '*.js',
+                dest: 'tmp/js/min'
+            }
+        }
+    });
+
+    grunt.loadTasks('tasks');
+
+    // put more complex build tasks here 
+    // 1. remove all temp and release folders so that we are starting from scratch
+    // 2. generate flat html files from our templates
+    // 3. copy bootstrap (sass), jquery and other bower_components into the proper assets folder of our tmp folder (this is how you get them local)
+    // 4. copy over and merge boostrap js assets in the proper order (here you can pick and choose)
+    // 5. run sass processor against our scss files (generates css files)
+    // 6. minify our javascript files (only the ones that aren't min the min folder already) and move them into the min folder
+    // 7. merge our css files together
+    // 8. merge our js files together (from min folder) - jquery first and place the output into our release folder
+    // 9. minify our css file and place the output into our release folder
+    // 10. finally clean up the temp folder again as it's no longer needed
+    grunt.registerTask('base', ['clean:all', 'templates', 'copy', 'concat:bootstrap', 'sass', 'uglify', 'concat:css', 'concat:js', 'cssmin']);
+
+    // high level order of operations for infrastructure stuff
+    grunt.registerTask('default', ['base', 'connect', 'open', 'watch']);
+};
+
---
+++ tasks/template.js
@@ -0,0 +1,249 @@
+'use strict';
+var path = require('path');
+var chalk =  require('chalk');
+var lorem = require('lorem');
+
+module.exports = function (grunt) {
+    grunt.registerMultiTask('templates', 'Inject templates into your flat html files', function(){
+        var options = this.options({
+            src: '',
+            dest: '',
+            templateDir: ''
+        });
+
+        for(var prop in options){
+            if(options[prop] == '')grunt.fatal('options.' + prop + ' is required');
+        }
+
+        if(!grunt.file.exists(options.src))grunt.fatal('options.src points to a non-existant dir: ' + options.src);
+        if(!grunt.file.exists(options.templateDir))grunt.fatal('options.templateDir points to a non-existant dir: ' + options.templateDir);
+
+        var files = grunt.file.expand({ cwd: options.src }, '*.html');
+        if(files.length == 0){
+            grunt.log.warn('no html files found in directory ' + chalk.cyan(options.src));
+            grunt.log.warn('aborting as there are no files to parse');
+            return;
+        }
+
+        var templates = grunt.file.expand({ cwd: options.templateDir }, '**/*.html');
+      
+        templates.forEach(function(template){
+            var name = template.replace('.html', '');
+            grunt.log.ok(name);
+            var html = grunt.file.read(path.join(options.templateDir, template));
+            templates[name] = html;
+        });
+
+        files.forEach(function(file){
+            var model = {};
+
+            grunt.log.subhead('[*] processing ' + chalk.cyan(file));
+            var html = grunt.file.read(path.join(options.src, file));
+
+            grunt.log.subhead('parsing model');
+            html = parseModel(html, model);
+
+            grunt.log.subhead('parsing blocks');
+            html = parseBlock(html, {
+                matchExpression: /{{(?!model)(.*?)}}((.|\r\n)*?){{\/\1}}/gmi,
+                isBlock: true
+            }, model);
+
+            grunt.log.subhead('parsing inline');
+            html = parseBlock(html, {
+                matchExpression: /{{(?!model|repeat|\/repeat|ipsum|img:)(.*?)(?:)}}/gmi
+            }, model);
+
+            grunt.log.subhead('parsing model properties');
+            html = parseModelProperties(html, model);
+
+            grunt.log.subhead('parsing lorem ipsum');
+            html = parseIpsumBlock(html);
+
+            grunt.log.subhead('parsing img');
+            html = parseImgBlock(html);
+
+            grunt.file.write(path.join(options.dest, file), html);
+        });
+
+        function parseBlock(html, options, model){
+            options = options || {};
+
+            var required = ['matchExpression'];
+            required.forEach(function(req){
+                if(!options[req]){
+                    grunt.log.error('parseBlock.' + chalk.cyan(req) + ' is required');    
+                    return html;
+                }
+            });
+
+            var matches = null;
+            while((matches = options.matchExpression.exec(html)) != null){
+                grunt.log.subhead('parsing model');
+                html = parseModel(html, model);
+
+                var search = matches[0];
+                var name = matches[1];
+                var innerContent = matches[2];
+
+                var content = '';
+
+                grunt.log.writeln('\tinjecting template ' + chalk.cyan(name));
+                if(!templates[name]){
+                    grunt.log.writeln('\t\t* template ' + chalk.cyan(name) + ' not found. Using an empty string as a replacement');
+                }else{
+                    if(options.isBlock){
+                        if(!options.yield)options.yield = 'yield';
+                        content = templates[name].replace('{{' + options.yield + '}}', innerContent);
+
+                        content = parseYields(content, {
+                            name: name
+                        });
+                    }else{
+                        content = templates[name];
+                    }
+                }
+
+                html = html.replace(new RegExp(search, 'g'), content);
+                grunt.log.subhead('parsing repeat blocks');
+                html = parseRepeatBlock(html);
+                
+                options.matchExpression.lastIndex = 0;
+            }
+
+            html = parseModel(html, model);
+
+            return html;
+        }
+
+        function parseRepeatBlock(html){
+            var matchExpression = /{{repeat:([0-9]*?)}}((.|\r\n)*?){{\/repeat}}/gmi;
+            var matches;
+
+            while((matches = matchExpression.exec(html)) != null){
+                var search = matches[0];
+                var times = parseInt(matches[1]);
+                var innerContent = matches[2];
+
+                var tmp = '';
+
+                for(var i=0;i<times;i++){
+                    tmp += innerContent;
+                }
+
+                html = html.replace(new RegExp(search, 'g'), tmp);
+            }
+            return  html;
+        }
+
+        function parseIpsumBlock(html){
+            var matchExpression = /{{ipsum:(.*?)}}/gmi;
+            var matches;
+
+            while((matches = matchExpression.exec(html)) != null){
+                var search = matches[0];
+                var command = matches[1];
+
+                var ipsum = lorem.ipsum(command);
+                html = html.replace(new RegExp(search, 'g'), ipsum);
+            }
+
+            return html;
+        }
+
+        function parseImgBlock(html){
+            //TODO some how make the attributes conditional/optional and use a single regex
+
+            var matchExpression = /{{img:(.*?):(.*?)}}/gmi;
+            var matches;
+
+            while((matches = matchExpression.exec(html)) != null){
+                var search = matches[0];
+                var command = matches[1];
+                var attributes = matches[2];
+
+                if(!attributes)attributes = '';
+
+                var img = '<img src="http://placehold.it/' + command + '" ' + attributes + ' >';
+
+                html = html.replace(new RegExp(search, 'g'), img);
+            }
+
+            matchExpression = /{{img:(.*?)}}/gmi;
+            matches;
+
+            while((matches = matchExpression.exec(html)) != null){
+                var search = matches[0];
+                var command = matches[1];
+
+                var img = '<img src="http://placehold.it/' + command + '" >';
+
+                html = html.replace(new RegExp(search, 'g'), img);
+            }
+
+            return html;
+        }
+
+        function parseYields(html, options){
+            
+            var matchExpression = /{{yield:(.*?)}}/gmi;
+            var matches = null;
+
+            while((matches = matchExpression.exec(html)) != null){
+                var search = matches[0];
+                var name = matches[1];
+
+                var block = extractBlock(html, '{{' + options.name + ':' + name + '}}((.|\r\n)*?){{/' + options.name + ':' + name + '}}');
+                if(block != null){
+                    var innerContent = block[1];
+                    html = html.replace(search, innerContent);
+                    matchExpression.lastIndex = 0;
+                }
+            }
+            return html;
+        }
+
+        function extractBlock(html, yieldStatement){
+            var matchExpression = new RegExp(yieldStatement, 'gmi');
+            var matches = matchExpression.exec(html);
+
+            return matches;
+        }
+         
+        function parseModel(html, model){
+            var matches = extractBlock(html, '{{model}}((.|\r\n)*?){{/model}}');
+            if(matches != null){
+                var search = matches[0];
+                var modelstr = matches[1];
+
+                var obj;
+                eval('obj = ' + modelstr);
+                
+                for(var prop in obj){
+                    model[prop] = obj[prop];
+                }
+
+                //todo for some reason the regex here failed when a model contained a function
+                html = html.replace(search, '');
+            }
+            
+            return html;
+        }
+
+        function parseModelProperties(html, model){
+            for(var prop in model){
+                var matchExpression = new RegExp('{{model.' + prop + '}}', 'gi');
+
+                var content = '';
+                if(typeof model[prop] == 'function'){
+                    content = model[prop]();
+                }else{
+                    content = model[prop];
+                }
+                html = html.replace(matchExpression, content);
+            }
+
+            return html;
+        }
+    });
+};
